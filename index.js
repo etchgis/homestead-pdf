33d8ffdb4b993e3af924f5ecfb655ebab85db57c
Malcolm Meyer <malcolmtmeyer@gmail.com>
2024-03-05 18:57:11 -0500

    updated fixed but not reading env variables in node

--- index.js
+++ index.js
@@ -11,18 +11,19 @@ const DEBUG = process.env.DEBUG ? true : false;
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const argv = process.argv.slice(2);
-const [id, dest] = argv;
+const [id] = argv;
 if (DEBUG) console.log({id, __dirname});
 
 (async () => {
 
   try {
-    const data = process.env.DEV ? JSON.parse(fs.readFileSync(path.join(__dirname, "lib/sampleData.json"))) : JSON.parse(fs.readFileSync(`c:/inetpub/internal/homestead-admin/applications/${id}.json`));
+    const data = process.env.DEV ? JSON.parse(fs.readFileSync(path.join(__dirname, "lib/sampleData.json"))) : JSON.parse(JSON.parse(fs.readFileSync(`c:/inetpub/internal/homestead-admin/applications/${id}.json`)));
     if (DEBUG) console.log({data})
-    const created = await fillPDF(data, dest);
+    const created = await fillPDF(data, process.env.DESTDIR);
     console.log({ created });
     if (created && !process.env.DEV) {
-      fs.unlinkSync(path.join(process.env.DESTDIR, `${id}.json`));
+      const DESTDIR = process.env.DESTDIR || "c:/inetpub/internal/homestead-admin/applications";
+      fs.unlinkSync(path.join(DESTDIR, `${id}.json`));
     }
   } catch (error) {
     console.error(error.toString());
