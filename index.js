a70626d605edde96d0e92e470211bb80611f7837
Malcolm Meyer <malcolmtmeyer@gmail.com>
2024-03-14 11:58:06 -0400

    patch: flattened PDF and add marital checkboxes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import * as fs from "fs"
+import * as fs from "fs";
 
 import dotenv from "dotenv";
-import { fileURLToPath } from 'url'
+import { fileURLToPath } from "url";
 import fillPDF from "./lib/fillPDF.js";
 import path from "path";
 
@@ -12,13 +12,16 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const argv = process.argv.slice(2);
 const [id] = argv;
-if (DEBUG) console.log({id, __dirname});
+if (DEBUG) console.log({ id, __dirname });
 
 (async () => {
-
   try {
-    const data = process.env.DEV ? JSON.parse(fs.readFileSync(path.join(__dirname, "lib/sampleData.json"))) : JSON.parse(JSON.parse(fs.readFileSync(`c:/inetpub/internal/homestead-admin/applications/${id}.json`)));
-    if (DEBUG) console.log({data})
+    const data = process.env.DEV
+      ? JSON.parse(fs.readFileSync(path.join(__dirname, "lib/sampleData.json")))
+      : JSON.parse(
+          JSON.parse(fs.readFileSync(`c:/inetpub/internal/homestead-admin/applications/${id}.json`))
+        );
+    if (DEBUG) console.log({ data });
     const created = await fillPDF(data, process.env.DESTDIR);
     console.log({ created });
     if (created && !process.env.DEV) {
@@ -27,8 +30,11 @@ if (DEBUG) console.log({id, __dirname});
     }
   } catch (error) {
     console.error(error.toString());
-    const timestamp = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '')
-    fs.appendFileSync(path.join(__dirname,  "error-log.log"), "\n" + timestamp + ": " + error.toString(), 'utf8')
+    const timestamp = new Date().toISOString().replace(/T/, " ").replace(/\..+/, "");
+    fs.appendFileSync(
+      path.join(__dirname, "error-log.log"),
+      "\n" + timestamp + ": " + error.toString(),
+      "utf8"
+    );
   }
-
-})()
+})();
